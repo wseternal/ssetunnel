88e10c636fe5ea12c8fa0eba510aabe34332b017
Cid (Zhaohua) Jiang <zhaohua@doublefin.com>
2026-07-25 22:02:33 +0800

    fix(local): remove debug echo in local.sh

--- local.sh
+++ local.sh
@@ -3,7 +3,6 @@
 LOCALDB=""
 
 if [[ "$@" =~ (^|[[:space:]])(-l|--local)($|[[:space:]]) ]]; then
-  echo "Found -l or --local"
   LOCALDB="env DB_USER=${DB_USER:-$USER} DB_NAME=ssetunnel DB_PASSWORD= DB_URL_TEMPLATE=postgres://[username]:[password]@[host]:[port]/[database_name]?sslmode=disable"
 fi
 
